444ec1e2618ed36dcd87eb83436807fa1e8cfdcf
Abhishekk24 <123966857+Abhishekk24@users.noreply.github.com>
2024-01-19 10:18:22 +0530

    analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,41 @@
-import { Raleway } from 'next/font/google'
-import './globals.css'
+// pages/_app.js (or pages/_app.tsx for TypeScript)
+
+import '../styles/globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { ThemeProvider } from '@/components/ThemeProvider'
-const inter = Raleway({ subsets: ['latin'] })
+import Head from 'next/head'; // Import Head from next
 
 export const metadata = {
   title: 'Vinr Enterprises',
   description: 'Business App',
 }
 
-export default function RootLayout({ children }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-
-      <body className={inter.className}><ThemeProvider attribute='class' defaultTheme='light'>
+    <>
+      <Head>
+        {/* Add your Google Analytics script here */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8PFT7ND6TC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8PFT7ND6TC');
+            `,
+          }}
+        />
+      </Head>
+      
+      <ThemeProvider attribute='class' defaultTheme='light'>
         <Header />
-        {children}
+        <Component {...pageProps} />
         <Footer />
       </ThemeProvider>
-
-      </body>
-
-    </html>
+    </>
   )
 }
+
+export default MyApp
